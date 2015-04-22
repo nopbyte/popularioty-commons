af8f48bbc3cba99eb6293903b80c0de36dfd379b
nopbyte <juandabytes@gmail.com>
2015-04-22 14:52:13 +0200

    Start the initiative to have all type of entities in one place... ;)

---
+++ src/main/java/popularioty/commons/constants/EntityTypeConstants.java
@@ -0,0 +1,9 @@
+package popularioty.commons.constants;
+
+public interface EntityTypeConstants 
+{
+	public static String ENTITY_TYPE_SO_STREAM = "service_object_stream";
+	public static String ENTITY_TYPE_SO = "service_object";
+	public static String ENTITY_TYPE_SERVICE = "service_instance";
+	public static String ENTITY_USER= "user";
+}
